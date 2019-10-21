0d48bd44216660053e879fcf5cea9755304d083c
Hissa <hissa7909@hotmail.com>
2019-10-21 23:22:53 +0300

    add t code without execution

---
+++ app/src/main/java/p/fetch/TweetItemActivity.java
@@ -0,0 +1,14 @@
+package p.fetch;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class TweetItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tweet_item);
+    }
+}
